4f0264befa3b5d101f5bc6bbf95b205e559c6ac2
Zimolev <zimolev@gmail.com>
2023-02-17 11:19:36 +0400

    The end project

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,22 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String[] massiv  = {"Winter", "Spring", "Summer", "Autumn", "January", "February", "March", "April",
+                            "May", "June", "July", "August", "September", "October", "November", "November", " "};
+        int sizeMassivTwo = 0;
+        for (int i = 0; i < massiv.length; i++) {
+            int lenghtWord = massiv[i].length();
+            if (lenghtWord <=3) sizeMassivTwo = sizeMassivTwo + 1;
+        }        String[] massivThreeSimbol = new String[sizeMassivTwo];
+        for (int i = 0, j = 0; i < massiv.length; i++) {
+            int lenghtWord = massiv[i].length();
+            if (lenghtWord <=3)
+            {
+                massivThreeSimbol[j] = massiv[i];
+                System.out.print("'" + massivThreeSimbol[j] + "'" + " ");
+                j = j + 1;
+            }
+        }
     }
+
 }
